ef561b7d8b584ce6ca8a93a2c08bf682df68d121
sugawara kensuke <superradio86@gmail.com>
2023-06-30 21:24:06 -0400

    add test for equality

--- currency/bin/money.dart
+++ currency/bin/money.dart
@@ -1,7 +1,17 @@
 class Doller {
   int amount;
   Doller(this.amount);
-  void times(int multiplier) {
-    amount *= multiplier;
+  Doller times(int multiplier) {
+    //amount *= multiplier;
+    return Doller(amount * multiplier);
   }
+
+  @override
+  bool operator ==(Object other) =>
+      other is Doller &&
+      other.runtimeType == runtimeType &&
+      other.amount == amount;
+
+  @override
+  int get hashCode => amount.hashCode;
 }
--- currency/bin/money_test.dart
+++ currency/bin/money_test.dart
@@ -5,7 +5,12 @@ import 'money.dart';
 void main() {
   test('Multiplication', () {
     var five = Doller(5);
-    five.times(2);
-    expect(five.amount, equals(10));
+    var product = five.times(2);
+    expect(product.amount, equals(10));
+    product = five.times(3);
+    expect(product.amount, equals(15));
+  });
+  test('Equality', () {
+    expect(Doller(5), equals(Doller(5)));
   });
 }
