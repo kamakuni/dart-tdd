d8c6b0119745dc9ca6fdb7426e9247ee87390d04
sugawara kensuke <superradio86@gmail.com>
2023-07-01 17:19:32 -0400

    modify amount field

--- currency/bin/money.dart
+++ currency/bin/money.dart
@@ -1,17 +1,17 @@
 class Doller {
-  int amount;
-  Doller(this.amount);
+  final int _amount;
+  Doller(this._amount);
   Doller times(int multiplier) {
     //amount *= multiplier;
-    return Doller(amount * multiplier);
+    return Doller(_amount * multiplier);
   }
 
   @override
   bool operator ==(Object other) =>
       other is Doller &&
       other.runtimeType == runtimeType &&
-      other.amount == amount;
+      other._amount == _amount;
 
   @override
-  int get hashCode => amount.hashCode;
+  int get hashCode => _amount.hashCode;
 }
