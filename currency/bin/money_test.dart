4d35b376d13c709de86b2492d262f3f174c69a44
sugawara kensuke <superradio86@gmail.com>
2023-07-01 17:13:05 -0400

    modify test for multiplication

--- currency/bin/money_test.dart
+++ currency/bin/money_test.dart
@@ -4,11 +4,9 @@ import 'money.dart';
 
 void main() {
   test('Multiplication', () {
-    var five = Doller(5);
-    var product = five.times(2);
-    expect(product.amount, equals(10));
-    product = five.times(3);
-    expect(product.amount, equals(15));
+    final five = Doller(5);
+    expect(five.times(2), equals(Doller(10)));
+    expect(five.times(3), equals(Doller(15)));
   });
   test('Equality', () {
     expect(Doller(5), equals(Doller(5)));
