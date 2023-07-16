3bb3a947d8d5cc62281e9d18141bfceb3c0cbf5d
sugawara kensuke <superradio86@gmail.com>
2023-07-15 23:35:55 -0400

    remove unnecessary test codes

--- currency/bin/money_test.dart
+++ currency/bin/money_test.dart
@@ -11,14 +11,7 @@ void main() {
   test('Equality', () {
     expect(Money.doller(5), equals(Money.doller(5)));
     expect(Money.doller(5), isNot(equals(Money.doller(6))));
-    expect(Money.franc(5), equals(Money.franc(5)));
-    expect(Money.franc(5), isNot(equals(Money.franc(6))));
     expect(Money.doller(5), isNot(equals(Money.franc(5))));
-  });
-  test('FrancMultiplication', () {
-    final five = Money.franc(5);
-    expect(five.times(2), equals(Money.franc(10)));
-    expect(five.times(3), equals(Money.franc(15)));
   });
   test('Currency', () {
     expect(Money.doller(1).currency, equals("USD"));
