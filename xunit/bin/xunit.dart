567ee70295591255fa49c465f803643a66a3e3d2
sugawarakensuke <superradio86@gmail.com>
2023-09-21 00:01:43 +0900

    update condition for asserts

--- xunit/bin/xunit.dart
+++ xunit/bin/xunit.dart
@@ -3,8 +3,8 @@ import 'package:xunit/xunit.dart';
 void main(List<String> arguments) {
   var test = WasRun("testMethod");
   print(test.wasRun);
-  assert(!test.wasRun);
+  assert(test.wasRun != 1);
   test.run();
-  assert(test.wasRun);
+  assert(test.wasRun == 1);
   print(test.wasRun);
 }
