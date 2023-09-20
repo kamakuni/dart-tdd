fe087692d3026c3c77fc87195c9d20a5c247e45c
sugawarakensuke <superradio86@gmail.com>
2023-09-21 00:00:01 +0900

    add asserts

--- xunit/bin/xunit.dart
+++ xunit/bin/xunit.dart
@@ -3,6 +3,8 @@ import 'package:xunit/xunit.dart';
 void main(List<String> arguments) {
   var test = WasRun("testMethod");
   print(test.wasRun);
+  assert(!test.wasRun);
   test.run();
+  assert(test.wasRun);
   print(test.wasRun);
 }
