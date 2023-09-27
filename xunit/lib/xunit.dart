5d24fcb949619f98d8f2d3e0aefdf25a9ac2b21a
sugawarakensuke <superradio86@gmail.com>
2023-09-27 23:59:36 +0900

    refactor setUp method

--- xunit/lib/xunit.dart
+++ xunit/lib/xunit.dart
@@ -3,6 +3,7 @@ import 'dart:mirrors';
 class TestCase {
   final String name;
   TestCase(this.name);
+  void setUp() {}
   void run() {
     final m = reflect(this);
     m.invoke(Symbol(name), []);
@@ -14,6 +15,7 @@ class WasRun extends TestCase {
   int? wasSetUp;
   WasRun(name): super(name);  
 
+  @override
   void setUp(){
     wasRun = null;
     wasSetUp = 1;
@@ -28,17 +30,21 @@ class WasRun extends TestCase {
 class TestCaseTest extends TestCase {
   TestCaseTest(super.name);
 
+  late TestCase _test;
+  @override
+  void setUp() {
+    _test = WasRun("testMethod");
+  }
+
   void testRunning() {
     var test = WasRun("testMethod");
-    assert(test.wasRun != 1);
-    test.run();
+    _test.run();
     assert(test.wasRun == 1);
   }
 
   void testSetUp() {
     var test = WasRun("testMethod");
-    assert(test.wasRun != 1);
-    test.run();
+    _test.run();
     assert(test.wasRun == 1);
   }
 }
