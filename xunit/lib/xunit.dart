d0c2c17545a7e392a598ac953182e4a2c1987cf5
sugawarakensuke <superradio86@gmail.com>
2023-09-29 22:18:18 +0900

    fix fields

--- xunit/lib/xunit.dart
+++ xunit/lib/xunit.dart
@@ -2,17 +2,19 @@ import 'dart:mirrors';
 
 class TestCase {
   final String name;
+  int? wasRun;
+  int? wasSetUp;
   TestCase(this.name);
   void setUp() {}
   void run() {
+    setUp();
     final m = reflect(this);
     m.invoke(Symbol(name), []);
   }
 }
 
 class WasRun extends TestCase {
-  int? wasRun;
-  int? wasSetUp;
+
   WasRun(name): super(name);  
 
   @override
@@ -37,13 +39,11 @@ class TestCaseTest extends TestCase {
   }
 
   void testRunning() {
-    _test = WasRun("testMethod");
     _test.run();
     assert(_test.wasRun == 1);
   }
 
   void testSetUp() {
-    _test = WasRun("testMethod");
     _test.run();
     assert(_test.wasRun == 1);
   }
