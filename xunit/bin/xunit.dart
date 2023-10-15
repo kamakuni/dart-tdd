8593a83fd8e2cbd36dabcdfc3cd7cd763e9104bd
sugawarakensuke <superradio86@gmail.com>
2023-10-15 22:16:08 +0900

    add test case call

--- xunit/bin/xunit.dart
+++ xunit/bin/xunit.dart
@@ -3,4 +3,5 @@ import 'package:xunit/xunit.dart';
 void main(List<String> arguments) {
   TestCaseTest("testTemplateMethod").run();
   TestCaseTest("testResult").run();
+  TestCaseTest("testFailedResult").run();
 }
