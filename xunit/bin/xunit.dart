33498b6398042695d58f77ff42ba278f3ea2a862
sugawarakensuke <superradio86@gmail.com>
2023-11-12 22:20:11 +0900

    remove .run().summary()

--- xunit/bin/xunit.dart
+++ xunit/bin/xunit.dart
@@ -1,9 +1,9 @@
 import 'package:xunit/xunit.dart';
 
 void main(List<String> arguments) {
-  print(TestCaseTest("testTemplateMethod").run().summary());
-  print(TestCaseTest("testResult").run().summary());
-  print(TestCaseTest("testFailedResult").run().summary());
-  print(TestCaseTest("testFailedResultFormatting").run().summary());
-  print(TestCaseTest("testSuite").run().summary());
+  print(TestCaseTest("testTemplateMethod"));
+  print(TestCaseTest("testResult"));
+  print(TestCaseTest("testFailedResult"));
+  print(TestCaseTest("testFailedResultFormatting"));
+  print(TestCaseTest("testSuite"));
 }
